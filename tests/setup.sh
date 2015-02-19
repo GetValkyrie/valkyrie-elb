0cc493e1be18a756b56eef4ca7777644f7868e5a
Antoine Beaupré <anarcat@koumbit.org>
2015-02-19 16:19:26 -0500

    setup boto and roles properly

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,3 +1,5 @@
 #/bin/sh
 
 curl https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh | /bin/sh
+cd /etc/ansible/roles && ln -s /vagrant /etc/ansible/roles/valkyrie-elb
+cd /vagrant && git update-index --assume-unchanged boto.cfg
