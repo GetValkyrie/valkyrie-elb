2a2e422e5cd8999a1806ce231883768944285116
Antoine Beaupré <anarcat@koumbit.org>
2015-02-19 16:56:54 -0500

    make this actually work

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,5 +1,10 @@
 #/bin/sh
 
+export ANSIBLE_CHECKOUT=v1.8.4
 curl https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh | /bin/sh
-cd /etc/ansible/roles && ln -s /vagrant /etc/ansible/roles/valkyrie-elb
+mkdir -p /etc/ansible/roles && cd /etc/ansible/roles && ln -s /vagrant /etc/ansible/roles/valkyrie-elb
+apt-get -y install python-pip
+# the debian package is too old for ansible 1.8
+pip install boto
+cp /vagrant/boto.cfg /etc/boto.cfg
 cd /vagrant && git update-index --assume-unchanged boto.cfg
